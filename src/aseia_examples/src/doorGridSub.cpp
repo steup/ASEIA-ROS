b3fc9b46a7c08e5c6349d981b9de6e9095074d9f
Dirk Steindorf <dirk.steindorf@gmail.com>
2015-12-09 16:01:47 +0100

    DoorGrids are forwarded to another ROS topic

--- src/aseia_examples/src/doorGridSub.cpp
+++ src/aseia_examples/src/doorGridSub.cpp
@@ -1,10 +1,16 @@
 #include <ros/ros.h>
 #include <ros/console.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/Quaternion.h>
+#include <nav_msgs/OccupancyGrid.h>
 
 #include <SensorEventSubscriber.h>
 #include <BaseEvent.h>
 
 using namespace id::attribute;
+
+ros::Publisher pub;
 
 struct DoorGridEventConfig : public BaseConfig
 {
@@ -16,8 +22,33 @@ struct DoorGridEventConfig : public BaseConfig
 using DoorGridAttribute = Attribute<OccupancyGrid, Value<uint8_t, 24, 24, false>, Dimensionless>;
 using DoorGridEvent = BaseEvent<DoorGridEventConfig>::append<DoorGridAttribute>::type;
 
-void print(const DoorGridEvent& e){
-  ROS_INFO_STREAM("received: " << e);
+
+void forward(const DoorGridEvent& e){
+  nav_msgs::OccupancyGrid msg;
+  //ROS_INFO_STREAM("received: " << e);
+
+  ROS_INFO_STREAM("grid = " << e.attribute(OccupancyGrid()).value()(0,0));
+  for(unsigned int y=0; y < 24; y++){
+    for(unsigned int x=0; x < 24; x++){
+      msg.data.push_back(e.attribute(OccupancyGrid()).value()(x,y).value());
+    }
+  }
+
+  msg.header.stamp           = ros::Time::now();
+
+  //msg.info.map_load_time     =
+  msg.info.resolution        = 0.05f;
+  msg.info.width             = 24;
+  msg.info.height            = 24;
+  msg.info.origin.position.x = e.attribute(Position()).value()(0);
+  msg.info.origin.position.y = e.attribute(Position()).value()(1);
+  msg.info.origin.position.z = 0;
+  //msg.info.origin.quaternion.x
+  //msg.info.origin.quaternion.y
+  //msg.info.origin.quaternion.z
+  //msg.info.origin.quaternion.w
+
+  pub.publish(msg);
 }
 
 int main(int argc, char** argv){
@@ -25,10 +56,14 @@ int main(int argc, char** argv){
 
   ROS_INFO_STREAM("started");
 
-  SensorEventSubscriber<DoorGridEvent> sub("door", 1, print);
+  SensorEventSubscriber<DoorGridEvent> sub("door", 1, forward);
+
+  ros::NodeHandle pubNode;
+  pub = pubNode.advertise<nav_msgs::OccupancyGrid>("map", 1000);
+
 
   while(ros::ok())
     ros::spin();
-    
+
   return 0;
 }
