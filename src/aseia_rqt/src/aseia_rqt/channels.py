b3c8c1c0d768fda29258e24d388c1ac0570cbd1e
Christoph Steup <steup@ivs.cs.ovgu.de>
2016-07-19 00:26:14 +0200

    Prototype of rqt ASEIA Channel UI:
            - no working transform channels yet
            - no real implementation besides interface

--- src/aseia_rqt/src/aseia_rqt/channels.py
+++ src/aseia_rqt/src/aseia_rqt/channels.py
@@ -1,17 +1,27 @@
+import os
+
+import rospkg
+
 from qt_gui.plugin import Plugin
-from python_qt_binding.QtWidgets import QWidget
+from python_qt_binding import loadUi
+from python_qt_binding.QtWidgets import QWidget, QListView
+from python_qt_binding.QtCore import QStringListModel, QVariant
+
 
 class ChannelsGUI(Plugin):
 
     def __init__(self, context):
         super(ChannelsGUI, self).__init__(context)
+        self.channels = QStringListModel()
         self.setObjectName('Channels')
         self._widget = QWidget()
-        self._widget.setObjectName('MyPluginUi')
-        self._widget.setWindowTitle('ASEIA Channels')
+        ui_file = os.path.join(rospkg.RosPack().get_path('aseia_rqt'), 'common', 'channels.ui')
+        loadUi(ui_file, self._widget)
         if context.serial_number() > 1:
             self._widget.setWindowTitle(self._widget.windowTitle() + (' (%d)' % context.serial_number()))
         context.add_widget(self._widget)
+        self._widget.findChild(QListView, "ChannelList").setModel(self.channels)
+        self.channels.setStringList(["Test"])
 
     def shutdown_plugin(self):
         pass
