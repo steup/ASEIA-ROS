532a5c6d9c888fc696dda421308569fe6e1806b2
Christoph Steup <steup@ivs.cs.ovgu.de>
2016-08-25 01:37:22 +0200

    Asiea-Manager: Fixed Bug in RosChannel Constructor

--- src/aseia_base/src/manager_new.cpp
+++ src/aseia_base/src/manager_new.cpp
@@ -22,61 +22,59 @@
 
 
 std::string topic(EventID eID, FormatID fID) {
-	std::ostringstream os;
-	os << "/sensors/" << eID << "/" << fID;
-	return os.str();
+  std::ostringstream os;
+  os << "/sensors/" << eID << "/" << fID;
+  return os.str();
 }
 
 std::string topic(const EventType& eT) { return topic(eT, eT); }
 
 /*bool topic(const std::string& s, EventID& eID, FormatID& fID) {
-	std::istringstream is(s);
-	is >> "/sensors/" >> eID >> "/" >> fID;
-	return true;
+  std::istringstream is(s);
+  is >> "/sensors/" >> eID >> "/" >> fID;
+  return true;
 }*/
 
 class RosChannel : public Channel {
-	protected:
-		ros::Publisher mPub;
-		std::list<ros::Subscriber> mSubs;
+  protected:
+    ros::Publisher mPub;
+    std::list<ros::Subscriber> mSubs;
 
-	public:
-		void unpackEvent(aseia_base::SensorEvent::ConstPtr msgPtr, const EventType* eTPtr) {
-				MetaEvent  e=MetaFactory::instance().create(*eTPtr);
-				DeSerializer<decltype(msgPtr->event.begin())> d(msgPtr->event.begin(), msgPtr->event.end());
-				d >> e;
-				handleEvent(e);
-		}
+  public:
+    void unpackEvent(aseia_base::SensorEvent::ConstPtr msgPtr, const EventType* eTPtr) {
+        MetaEvent  e=MetaFactory::instance().create(*eTPtr);
+        DeSerializer<decltype(msgPtr->event.begin())> d(msgPtr->event.begin(), msgPtr->event.end());
+        d >> e;
+        handleEvent(e);
+    }
 
     RosChannel() = default;
 
-		RosChannel(TransPtr&& trans) : Channel(std::move(trans)) {
-			ros::NodeHandle n;
-			mPub =  n.advertise<aseia_base::SensorEvent>(topic(trans->out()), 1);
-			for(const EventType* in : trans->in()) {
-				mSubs.push_back(n.subscribe<aseia_base::SensorEvent>(topic(*in), 1, boost::bind(&RosChannel::unpackEvent,  this,  _1, in)));
-			}
-		}
+    RosChannel(TransPtr&& trans) : Channel(std::move(trans)) {
+      ros::NodeHandle n;
+      mPub =  n.advertise<aseia_base::SensorEvent>(topic(mTrans->out()), 1);
+      for(const EventType* in : mTrans->in()) {
+        mSubs.push_back(n.subscribe<aseia_base::SensorEvent>(topic(*in), 1, boost::bind(&RosChannel::unpackEvent,  this,  _1, in)));
+      }
+    }
 
-   // RosChannel(RosChannel&& movee) : Channel(std::move(movee)), mPub(std::move(movee.mPub)), mSubs(std::move(movee.mSubs)) {}
-
-		virtual void publishEvent(const MetaEvent& e) const {
-			aseia_base::SensorEvent sE;
-			Serializer<decltype(back_inserter(sE.event))> s(back_inserter(sE.event));
-			s << e;
-			mPub.publish(sE);
-		}
+    virtual void publishEvent(const MetaEvent& e) const {
+      aseia_base::SensorEvent sE;
+      Serializer<decltype(back_inserter(sE.event))> s(back_inserter(sE.event));
+      s << e;
+      mPub.publish(sE);
+    }
 };
 
 class ChannelManager {
-	private:
+  private:
     using PublisherRegistry = AbstractRegistry<EventType>;
     using ChannelRegistry = std::unordered_map<uint64_t, RosChannel>;
-		ros::Subscriber mSub;
+    ros::Subscriber mSub;
     ros::ServiceServer mPubSrv;
     PublisherRegistry mPubs;
     ChannelRegistry mChannels;
-	public:
+  public:
     bool providePublishers(aseia_base::Publishers::Request& req, aseia_base::Publishers::Response& res) {
       std::ostringstream os;
       for(const EventType& eT : mPubs)
@@ -85,29 +83,30 @@ class ChannelManager {
       return true;
     }
 
-		void handleNode(aseia_base::EventTypeConstPtr eTPtr) {
-			EventType eT;
-			DeSerializer<decltype(eTPtr->data.begin())> d(eTPtr->data.begin(), eTPtr->data.end());
-			d >> eT;
+    void handleNode(aseia_base::EventTypeConstPtr eTPtr) {
+      EventType eT;
+      DeSerializer<decltype(eTPtr->data.begin())> d(eTPtr->data.begin(), eTPtr->data.end());
+      d >> eT;
       ROS_INFO_STREAM("Got new " << (eTPtr->type==aseia_base::EventType::PUBLISHER?"Publisher":"Subscriber") <<
                       " with EventType " << eT << "on base topic " << eTPtr->topic);
       if(eTPtr->type == aseia_base::EventType::PUBLISHER)
         mPubs.registerType(eT, eT);
       else {
         for(Transformation::TransPtr p : TransformGenerator(eT, mPubs)) {
-					if(p) {
-          	mChannels.emplace(eTPtr->id, std::move(p));
-          	ROS_INFO_STREAM("Established Channel" << mChannels[eTPtr->id]);
-					} else
-						ROS_ERROR_STREAM("Invalid Channel, Transformer is NULL");
+          if(p) {
+            ROS_INFO_STREAM("Found Transform" << *p);
+            mChannels.emplace(eTPtr->id, std::move(p));
+            ROS_INFO_STREAM("Established Channel" << mChannels[eTPtr->id]);
+          } else
+            ROS_ERROR_STREAM("Invalid Channel, Transformer is NULL");
         }
       }
-		}
-		ChannelManager() {
-			ros::NodeHandle n;
-			mSub = n.subscribe("/sensors/management", 1, &ChannelManager::handleNode, this);
+    }
+    ChannelManager() {
+      ros::NodeHandle n;
+      mSub = n.subscribe("/sensors/management", 1, &ChannelManager::handleNode, this);
       mPubSrv = n.advertiseService("/sensors/publishers", &ChannelManager::providePublishers, this);
-		}
+    }
 };
 
 int main(int argc, char** argv){
