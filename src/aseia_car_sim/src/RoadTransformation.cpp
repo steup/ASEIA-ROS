c33bd20da2bc21fd1a0a6e04ac4e2b79636c4c5f
Christoph Steup <steup@ivs.cs.ovgu.de>
2017-03-13 17:31:52 +0100

    Aseia_Car_Sim: renamed roadtransform and filled it partially

--- src/aseia_car_sim/src/RoadTransformation.cpp
+++ src/aseia_car_sim/src/RoadTransformation.cpp
@@ -1,23 +1,60 @@
 #include <pluginlib/class_list_macros.h>
 #include <Transformation.h>
 
+#include <MetaEvent.h>
+
 #include <iostream>
 
 namespace aseia_car_sim {
 
-using namespace std;
+  using namespace std;
 
-class RoadTransformation : public Transformation {
-  public:
-    RoadTransformation() : Transformation(EventID::any) {}
-    virtual ~RoadTransformation() {}
-    virtual size_t arity() const { return 1; }
-    virtual EventIDs in(EventID goal) const { return EventIDs({goal}); };
-    virtual bool check(const EventType& out, const EventTypes& in) const { return false; }
-    virtual TransPtr create(const EventType& out, const EventTypes& in) const { return TransPtr(); }
-    virtual void print(ostream& o) const { o << "not yet implemented road to UTM transformation"; }
-};
+  class RoadToUTMTransformer : public Transformer {
+    private:
+      uint32_t mInRef, mOutRef;
+    public:
+      RoadToUTMTransformer(const Transformation* t, const EventType& out, const EventTypes& in)
+        : Transformer(t, out, in) {
+        //TODO Extract Scale Reference IDs
+      }
+
+      virtual bool check(const Events& events) const {
+        return true;
+      }
+
+      virtual MetaEvent operator()(const Events& events) {
+        return MetaEvent();
+      }
+
+      virtual  void print(ostream& o) const {
+        o << "Road("  << mInRef << ") to UTM(" << mOutRef << ") Transformer";
+      }
+  };
+
+  class RoadToUTMTransformation : public Transformation {
+    public:
+      RoadToUTMTransformation() : Transformation(EventID::any) {}
+
+      virtual size_t arity() const { return 2; }
+
+      virtual EventIDs in(EventID goal) const { 
+        //TODO Create Reference Event
+        return EventIDs({goal}); 
+      };
+
+      virtual bool check(const EventType& out, const EventTypes& in) const { 
+        return false; 
+      }
+
+      virtual TransPtr create(const EventType& out, const EventTypes& in) const { 
+        return TransPtr(new RoadToUTMTransformer(this, out, in)); 
+      }
+
+      virtual void print(ostream& o) const { 
+        o << "not yet implemented road to UTM transformation"; 
+      }
+  };
 
 }
 
-PLUGINLIB_EXPORT_CLASS(aseia_car_sim::RoadTransformation, Transformation)
+PLUGINLIB_EXPORT_CLASS(aseia_car_sim::RoadToUTMTransformation, Transformation)
