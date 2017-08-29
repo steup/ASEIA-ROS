2b70a735634a6120444a798ece0d31a2b2d94c46
Christoph Steup <steup@ivs.cs.ovgu.de>
2017-08-30 01:32:47 +0200

    Aseia_car_sim: Kalman infrastructure protoype

--- src/aseia_car_sim/src/VirtACCKalman.cpp
+++ src/aseia_car_sim/src/VirtACCKalman.cpp
@@ -10,18 +10,52 @@ namespace aseia_car_sim {
 
 using namespace std;
 using namespace ::id::attribute;
+using namespace ::id;
+
 class VirtACCKalmanTransformer : public Transformer {
+  private:
+    MetaValue extractValueFromFilter(ID attr, const MetaFilter& filter) {
+      for(const auto& expr : filter.expressions()) {
+        const MetaPredicate& p = expr.first;
+        if(p.attribute()==attr && p.constArgument())
+          return p.argument().value();
+        }
+      return MetaValue();
+    }
   public:
+    const type::ID type;
+    MetaValue x, P;
+    MetaValue time;
+    MetaValue speed0, speed1;
+    const MetaValue object, object2;
+    const MetaValue F1, B1, H1, Q, R;
     VirtACCKalmanTransformer(const EventType& goal, const EventTypes& in, const MetaFilter& filter)
-      : Transformer(goal, in) {
+      : Transformer(goal, in), type(goal[Distance()].value().typeId()),
+      object(extractValueFromFilter(Object(), filter)), object2(extractValueFromFilter(Object2(), filter)),
+      F1({{{0}}}, type), B1({{{0}}}, type),
+      H1({{{0}}}, type),
+      Q({{{0}}}, type), R({{{0}}}, type) {
     }
 
     virtual bool check(const MetaEvent& e) const {
       // todo check for compatibility of speed to state or of future of sensing to state
-      return true;
+      return ( e.attribute(Distance()) && e[Object()].value() == object && e[Object2()].value() == object2 ) ||
+             ( e.attribute(Speed()) && ( e[Object()].value() == object || e[Object()].value() == object2 ) );
     }
 
     virtual Events operator()(const MetaEvent& e) {
+      MetaValue z(type, 3, 1, true);
+      z = z.zero();
+      if(e.attribute(Distance())) {
+        z.block(0,0, e[Distance()].value());
+      }
+      if(e.attribute(Speed())) {
+        if(e[Object()].value()==object)
+          z.block(1,0, e[Speed()].value());
+        else
+          z.block(2,0, e[Speed()].value());
+      }
+      ROS_DEBUG_STREAM_NAMED("virt_acc_kalman", "z: " << z);
       //todo handle speed event or dist event / speed -> control, dist -> sensing
       if(e.attribute(Distance()))
         return {e};
@@ -37,7 +71,6 @@ class VirtACCKalmanTransformer : public Transformer {
 
 class VirtACCKalman : public Transformation {
   protected:
-    static const EventID sPositionID;
     static const EventID sDistID;
     static const EventID sSpeedID;
   public:
@@ -55,7 +88,7 @@ class VirtACCKalman : public Transformation {
     virtual EventIDs in(EventID goal, const MetaFilter& filter = MetaFilter()) const {
       if(!containsUncertaintyTest(filter)) return {};
       ROS_DEBUG_STREAM_NAMED("virt_kalman", "Kalman Input IDs for id " << goal);
-      return {sPositionID, sDistID, sSpeedID};
+      return {sDistID, sSpeedID};
     }
 
     virtual EventTypes in(const EventType& goal, const EventType& provided, const MetaFilter& filter = MetaFilter()) const {
@@ -67,8 +100,8 @@ class VirtACCKalman : public Transformation {
       const AttributeType& timeAttr = goal[Time()];
       EventType speed = position;
       AttributeType speedAttr(Speed(), distAttr.value(), MetaScale(distAttr.scale())/timeAttr.scale(), MetaUnit(distAttr.unit())/timeAttr.unit());
-      //todo define speed to use type of dist and position
-      return {position, speed, goal};
+      speed.add(speedAttr);
+      return {speed, goal};
     }
 
     virtual TransPtr create(const EventType& goal, const EventTypes& in, const AbstractPolicy& policy, const MetaFilter& filter= MetaFilter()) const {
@@ -79,9 +112,10 @@ class VirtACCKalman : public Transformation {
       o << "Kalman Transformation";
     }
 };
-const EventID VirtACCKalman::sPositionID=EventID(BaseEvent<>());
+
 const EventID VirtACCKalman::sDistID=EventID(BaseEvent<>())*Distance::value();
 const EventID VirtACCKalman::sSpeedID=EventID(BaseEvent<>())*Speed::value();
+
 }
 
 PLUGINLIB_EXPORT_CLASS(aseia_car_sim::VirtACCKalman, Transformation)
