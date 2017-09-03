671c5994587e918d9f2abd73549fec9319b46539
Christoph Steup <steup@ivs.cs.ovgu.de>
2017-09-03 05:12:45 +0200

    Aseia_car_sim: additional attributes adapted to new structure

--- src/aseia_car_sim/src/Attributes.h
+++ src/aseia_car_sim/src/Attributes.h
@@ -3,9 +3,5 @@
 #include <ID.h>
 
 namespace aseia_car_sim {
-
-  struct Nurbs : public id::attribute::Base{
-    static constexpr const id::attribute::ID value(){ return 100; }
-  };
-
+  using Nurbs = id::attribute::AttrID<100>;
 }
