b009cf844db7e7faa457b4db5bbdb5296eb16137
Christoph Steup <steup@ivs.cs.ovgu.de>
2017-09-13 19:50:13 +0200

    Aseia_Car_sim: add support for logging received aseia events

--- src/aseia_base/include/SensorEventSubscriber.h
+++ src/aseia_base/include/SensorEventSubscriber.h
@@ -7,6 +7,7 @@
 #include <ros/node_handle.h>
 
 #include <aseia_base/SensorEvent.h>
+#include <aseia_base/ReceiveIndication.h>
 
 #include <functional>
 
@@ -20,10 +21,17 @@ class SensorEventSubscriber<SensorEvent> : public AbstractPubSub<SensorEvent>  {
     using Base = AbstractPubSub<SensorEvent>;
 
     ros::Subscriber       mSub;
+    ros::Publisher        mIndicationPub;
+    aseia_base::ReceiveIndication mRecv;
     std::function<void (const SensorEvent&)> mCallback;
     void* mObjPtr;
 
     void unpack(const aseia_base::SensorEvent::ConstPtr& msg) {
+      mRecv.header.stamp=ros::Time::now();
+      mRecv.event = msg->event;
+      mRecv.topic = mSub.getTopic();
+      mIndicationPub.publish(mRecv);
+      mRecv.header.seq++;
       SensorEvent e;
       DeSerializer<decltype(msg->event.begin())> d(msg->event.begin(), msg->event.end());
       d >> e;
@@ -34,6 +42,8 @@ public:
   SensorEventSubscriber(void (*callback)(const SensorEvent&), size_t size=1)
     : Base(Base::Type::subscriber), mCallback(callback)
   {
+    mRecv.header.seq=0;
+    mIndicationPub = ros::NodeHandle().advertise<aseia_base::ReceiveIndication>("/recvIndication", 10);
     mSub = ros::NodeHandle().subscribe(this->topic(), size, &SensorEventSubscriber::unpack, this, ros::TransportHints().udp());
     ros::TimerEvent time;
     this->publishType(time);
@@ -43,6 +53,8 @@ public:
   SensorEventSubscriber(void (T::*callback)(const SensorEvent&), T* objPtr, size_t size=1)
     : Base(Base::Type::subscriber), mCallback(std::bind(callback, objPtr, std::placeholders::_1))
   {
+    mRecv.header.seq=0;
+    mIndicationPub = ros::NodeHandle().advertise<aseia_base::ReceiveIndication>("/recvIndication", 10);
     mSub = ros::NodeHandle().subscribe(this->topic(), size, &SensorEventSubscriber::unpack, this, ros::TransportHints().udp());
     ros::TimerEvent time;
     this->publishType(time);
@@ -55,11 +67,18 @@ class SensorEventSubscriber<SensorEvent, Filter> : public AbstractPubSub<SensorE
     using Base = AbstractPubSub<SensorEvent>;
 
     ros::Subscriber       mSub;
+    ros::Publisher        mIndicationPub;
+    aseia_base::ReceiveIndication mRecv;
     std::function<void (const SensorEvent&)> mCallback;
     void* mObjPtr;
     Filter mFilter;
 
     void unpack(const aseia_base::SensorEvent::ConstPtr& msg) {
+      mRecv.header.stamp=ros::Time::now();
+      mRecv.event = msg->event;
+      mRecv.topic = mSub.getTopic();
+      mIndicationPub.publish(mRecv);
+      mRecv.header.seq++;
       SensorEvent e;
       DeSerializer<decltype(msg->event.begin())> d(msg->event.begin(), msg->event.end());
       d >> e;
@@ -72,6 +91,8 @@ public:
     : Base(Base::Type::subscriber), mCallback(callback),
       mFilter(filter)
   {
+    mRecv.header.seq=0;
+    mIndicationPub = ros::NodeHandle().advertise<aseia_base::ReceiveIndication>("/recvIndication", 10);
     mSub = ros::NodeHandle().subscribe(this->topic(), size, &SensorEventSubscriber::unpack, this, ros::TransportHints().udp());
     auto filterImpl = filter(::filter::s0);
     auto& filterBuf = this->mTypeMsg.filter;
@@ -87,6 +108,8 @@ public:
     : Base(Base::Type::subscriber), mCallback(std::bind(callback, objPtr, std::placeholders::_1)),
       mFilter(filter)
   {
+    mRecv.header.seq=0;
+    mIndicationPub = ros::NodeHandle().advertise<aseia_base::ReceiveIndication>("/recvIndication", 10);
     mSub = ros::NodeHandle().subscribe(this->topic(), size, &SensorEventSubscriber::unpack, this, ros::TransportHints().udp());
     auto filterImpl = filter(::filter::s0);
     auto& filterBuf = this->mTypeMsg.filter;
