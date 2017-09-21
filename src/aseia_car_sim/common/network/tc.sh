46d1d3db7b4edf3ecd4d6ff2c97ca994ac74f9b5
Christoph Steup <steup@ivs.cs.ovgu.de>
2017-09-21 23:25:26 +0200

    Aseia_car_sim: fixes for lossy and utmtoroad configs

--- src/aseia_car_sim/common/network/tc.sh
+++ src/aseia_car_sim/common/network/tc.sh
@@ -4,9 +4,9 @@ sudo tc qdisc add dev lo root handle 1: htb default 1
 sudo tc class add dev lo parent 1: classid 1:2 htb rate 1Mbit
 sudo tc class add dev lo parent 1: classid 1:3 htb rate 1Mbit
 sudo tc class add dev lo parent 1: classid 1:4 htb rate 1Mbit
-sudo tc qdisc add dev lo parent 1:2 handle 2: netem loss 10
-sudo tc qdisc add dev lo parent 1:3 handle 3: netem delay 100ms 10ms
-sudo tc qdisc add dev lo parent 1:4 handle 4: netem loss 30 delay 1000ms 100ms
+sudo tc qdisc add dev lo parent 1:2 handle 2: netem loss 30
+sudo tc qdisc add dev lo parent 1:3 handle 3: netem delay 4000ms 400ms
+sudo tc qdisc add dev lo parent 1:4 handle 4: netem loss 30 delay 4000ms 400ms
 sudo tc filter add dev lo parent 1: protocol ip handle 2 fw classid 1:2
 sudo tc filter add dev lo parent 1: protocol ip handle 3 fw classid 1:3
 sudo tc filter add dev lo parent 1: protocol ip handle 4 fw classid 1:4
